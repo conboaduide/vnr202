fcf0db8d3889f138268e17932664bdf9cdef8b52
wintangz <wintang2003@icloud.com>
2025-03-16 02:30:33 +0700

    add new wallpaper, fix animation

--- app/boi-canh/page.jsx
+++ app/boi-canh/page.jsx
@@ -1,7 +1,18 @@
+import Image from 'next/image';
 import React from 'react';
 
 const BoiCanh = () => {
-  return <div>BoiCanh</div>;
+  return (
+    <div>
+      <Image
+        className='blur-sm -z-10 brightness-50'
+        src='/daithangmuaxuan.png'
+        alt='daithangmuaxuan'
+        layout='fill'
+        objectFit='cover'
+      />
+    </div>
+  );
 };
 
 export default BoiCanh;
--- app/page.jsx
+++ app/page.jsx
@@ -6,8 +6,8 @@ export default function Home() {
     <div>
       <Image
         className='blur-sm -z-10 brightness-50'
-        src='/daithangmuaxuan.png'
-        alt='daithangmuaxuan'
+        src='/dcsvn.png'
+        alt='dcsvn'
         layout='fill'
         objectFit='cover'
       />
@@ -19,16 +19,16 @@ export default function Home() {
         <div>QUÁ ĐỘ LÊN CHỦ NGHĨA XÃ HỘI</div>
         <div>VÀ TIẾN HÀNH CÔNG CUỘC ĐỔI MỚI</div>
       </div>
-      <div className='flex justify-center text-center mt-4'>
+      <div className='relative flex justify-center text-center mt-4'>
         <Link
           href='/boi-canh'
-          className='-z-10 group relative inline-block overflow-hidden rounded px-12 py-3 text-sm text-orange-100 font-medium'
+          className='group relative inline-block overflow-hidden rounded px-12 py-3 text-sm text-orange-100 font-medium hover:cursor-pointer select-none z-10'
         >
           Tìm hiểu
-          <span className='absolute left-0 top-0 w-full border-t-2 border-red-100 animate-top'></span>
-          <span className='absolute right-0 top-0 h-full border-r-2 border-red-100 animate-right'></span>
-          <span className='absolute bottom-0 right-0 w-full border-b-2 border-red-100 animate-bottom'></span>
-          <span className='absolute bottom-0 left-0 h-full border-l-2 border-red-100 animate-left'></span>
+          <span className='absolute left-0 top-0 w-0 border-t-2 border-red-100 animate-top'></span>
+          <span className='absolute right-0 top-0 h-0 border-r-2 border-red-100 animate-right'></span>
+          <span className='absolute bottom-0 right-0 w-0 border-b-2 border-red-100 animate-bottom'></span>
+          <span className='absolute bottom-0 left-0 h-0 border-l-2 border-red-100 animate-left'></span>
         </Link>
       </div>
     </div>
